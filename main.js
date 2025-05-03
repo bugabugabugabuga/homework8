c3f2c0b73f3a57c0e7ca2672226a7085dd23da93
bugabugabugabuga <Andria.sulkhanishvili@gmail.com>
2025-05-03 10:19:26 +0400

    homework10

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const express = require("express");
 const connectToDb = require("./db/db");
 const filmRouter = require("./routes/film.router");
 const directorrouter = require("./routes/director.router");
+const authRouter = require("../lec5/auth/auth.route");
 
 const app = express();
 
@@ -11,7 +12,7 @@ app.use(express.json());
 
 app.use("/films", filmRouter)
 app.use("/directors", directorrouter)
-
+app.use("/auth", authRouter)
 
 app.get("/", (req, res) => {
   res.send("hello");
